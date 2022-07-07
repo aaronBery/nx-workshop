c8dece67d057a1efae8d5cd1b0f9d9ea5ee5be5e
Aaron Bery <7768665+aaronBery@users.noreply.github.com>
2022-07-07 10:32:33 +0100

    chapter 6

---
+++ apps/store/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule, MatCardModule, StoreUiSharedModule, RouterModule.forRoot([{
+    path: 'game/:id',
+    loadChildren: () =>
+      import('@bg-hoard/store/feature-game-detail')
+        .then((module) => module.StoreFeatureGameDetailModule)
+  }])],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ libs/store/ui-shared/src/lib/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bg-hoard-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  @Input() title = 'Aaron';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
